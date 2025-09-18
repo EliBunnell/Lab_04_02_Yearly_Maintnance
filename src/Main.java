cdb553c88017081ccbec24c3c36d2b4effad4062
EliBunnell <bunnelec@mail.uc.edu>
2025-09-18 14:45:18 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+        double springCost = 10000;
+        double summerCost = 8000;
+        double fallCost = 6000;
+        double winterCost = 6000;
+        double annualCost = springCost + summerCost + fallCost + winterCost;
+
+        System.out.println("The cost for Spring is " + springCost + " and the cost for summer is " + summerCost + " and the cost for fall is " + fallCost + " and the winter cost is " + winterCost + " and the cost for annual is " + annualCost );
+    }
+}
